d30a384fe20c5b759a5cf5ef08e16c76f0d2c792
Kevin Kipkoech <105924200+Kevin7744@users.noreply.github.com>
2025-03-16 20:18:25 +0300

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# mcp-server-setup
-Simple project of how to setup an MCP (model context protocol) server.
---
+++ mcp-client/main.py
@@ -0,0 +1,33 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+
+
+mcp = FastMCP("docs")
+
+
+USER_AGENT = 'docs-app/1.0'
+SERPER_URL = "https://google.serper.dev/search"
+
+docs_urls = {
+    "langchain" : "python.langchain.com/docs",
+    "llama-index": "docs/llamaindex.ai/en/stable",
+    "openai": "platform.openai.com/docs"
+}
+
+
+
+def search_web():
+    
+def fetch_url():
+
+@mcp.tool    
+def get_docs():
+    
+    
+
+def main():
+    print("Hello from mcp-client!")
+
+
+if __name__ == "__main__":
+    main()
